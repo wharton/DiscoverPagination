ff6464220216b4131584214c2a5108589faf195f
Douglas H. King <dhking@wharton.upenn.edu>
2019-07-23 17:08:46 -0400

    Puts travis keys in global, reference values.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='DiscoverPagination',
-    version='0.1.3',
+    version='0.1.4',
     packages=setuptools.find_packages(),
     license='MIT',
     author_email="dhking@wharton.upenn.edu",
